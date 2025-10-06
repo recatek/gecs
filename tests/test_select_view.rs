4e7f4da3336300739c8b7871fd98f1ecade10184
recatek <recatek@hotmail.com>
2025-10-05 21:26:59 -0400

    Output consistency

--- tests/test_select_view.rs
+++ tests/test_select_view.rs
@@ -37,11 +37,11 @@ fn test_select_view() {
             assert_eq!(view.component::<CompA>(), &CompA(1));
             assert_eq!(view.component::<CompB>(), &CompB(10));
         }
-        SelectView::ArchBar(_) => panic!("Wrong view type"),
+        SelectView::ArchBar(_) => panic!("wrong view type"),
     }
 
     match world.view(entity_any_b).unwrap() {
-        SelectView::ArchFoo(_) => panic!("Wrong view type"),
+        SelectView::ArchFoo(_) => panic!("wrong view type"),
         SelectView::ArchBar(view) => {
             assert_eq!(view.component::<CompA>(), &CompA(2));
             assert_eq!(view.component::<CompC>(), &CompC(20));
